540e7df07fed327d0ecc24a593b6706ba4d554ae
Shauna Gordon-McKeon <shaunagm@gmail.com>
2023-06-17 18:19:58 -0400

    Update projects page, add new projects

--- generate_site.py
+++ generate_site.py
@@ -10,19 +10,19 @@ lookup = TemplateLookup(["./templates"])
 # Generate index page, talks, publications, project index page
 for page in ["index.html", "talks.html", "publications.html", "projects.html", "cv.html", "about.html"]:
     template = lookup.get_template("/" + page)
-    with open("./site/" + page, "wb") as newFile:
+    with open("./site/" + page, "w") as newFile:
         newFile.write(template.render(fullpath=fullpath))
 
 # Generate individual project pages
 for page in ["bongardcards.html", "eureka.html", "littler.html", "open-government-boston.html",
     "open-science-collaboration-blog.html", "open-source-recommendation-letter.html",
-    "smalltalk.html", "welcomebot.html", "when-women-refuse.html", "interface.html"]:
+    "smalltalk.html", "welcomebot.html", "when-women-refuse.html", "interface.html", "kybern.html"]:
     template = lookup.get_template("/projects/" + page)
-    with open("./site/projects/" + page, "wb") as newFile:
+    with open("./site/projects/" + page, "w") as newFile:
         newFile.write(template.render(fullpath=fullpath))
 
 # Generate misc pages
 for page in ["cakes.html"]:
     template = lookup.get_template("/misc/" + page)
-    with open("./site/misc/" + page, "wb") as newFile:
+    with open("./site/misc/" + page, "w") as newFile:
         newFile.write(template.render(fullpath=fullpath))
